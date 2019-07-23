f0456517bd636845bc31d757968aabe92e8c61f7
lsqgithub001 <225546147@qq.com>
2019-07-23 12:01:33 +0800

    创建工程

---
+++ myDianxiangOc/myDianxiangOc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myDianxiangOc
+//
+//  Created by 灰常溜 on 2019/7/23.
+//  Copyright © 2019 灰常溜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myDianxiangOc/myDianxiangOc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myDianxiangOc
+//
+//  Created by 灰常溜 on 2019/7/23.
+//  Copyright © 2019 灰常溜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
